c46783c3f77e9f6c8e48217ca5f7d0195443c611
aydın gültepe <aydngltp@gmail.com>
2018-12-09 16:00:13 +0300

    Median Filtreleme Eklendi.

--- GoruntuIsleme/Form1.Designer.cs
+++ GoruntuIsleme/Form1.Designer.cs
@@ -33,10 +33,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_uygula = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -50,9 +49,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(88, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(532, 440);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -60,67 +60,66 @@
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Location = new System.Drawing.Point(412, 57);
+            this.pictureBox2.Location = new System.Drawing.Point(624, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.Size = new System.Drawing.Size(532, 440);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(152, 319);
+            this.button1.Location = new System.Drawing.Point(147, 484);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = "Resim Yükle";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(483, 319);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Gri Yap";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button3
+            // comboBox1
             // 
-            this.button3.Location = new System.Drawing.Point(483, 348);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 20);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Binary Yap";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Gri Yap",
+            "Binary Yap",
+            "Sobel Kenar Uygula",
+            "Median Filtreleme Uygula"});
+            this.comboBox1.Location = new System.Drawing.Point(687, 486);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Seçiniz";
             // 
-            // button4
+            // btn_uygula
             // 
-            this.button4.Location = new System.Drawing.Point(483, 374);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(86, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Sobel Uygula";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.btn_uygula.Location = new System.Drawing.Point(948, 486);
+            this.btn_uygula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_uygula.Name = "btn_uygula";
+            this.btn_uygula.Size = new System.Drawing.Size(100, 28);
+            this.btn_uygula.TabIndex = 7;
+            this.btn_uygula.Text = "Uygula";
+            this.btn_uygula.UseVisualStyleBackColor = true;
+            this.btn_uygula.Click += new System.EventHandler(this.btn_uygula_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 461);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1161, 618);
+            this.Controls.Add(this.btn_uygula);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -136,10 +135,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_uygula;
     }
 }
 
--- GoruntuIsleme/Form1.cs
+++ GoruntuIsleme/Form1.cs
@@ -32,15 +32,6 @@ namespace GoruntuIsleme
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Bitmap image = new Bitmap(pictureBox1.Image);
-            Bitmap gri = griYap(image);
-
-            pictureBox2.Image = gri;
-        }
-
-
         private Bitmap griYap(Bitmap bmp)
         {
             for (int i = 0; i < bmp.Height - 1; i++)
@@ -65,13 +56,6 @@ namespace GoruntuIsleme
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Bitmap image = new Bitmap(pictureBox1.Image);
-            Bitmap binary = binaryYap(image);
-            pictureBox2.Image = binary;
-
-        }
 
         private Bitmap binaryYap(Bitmap image)
         {
@@ -97,19 +81,183 @@ namespace GoruntuIsleme
             return gri;
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Bitmap image = new Bitmap(pictureBox1.Image);
-            Bitmap sobel = sobelEdgeDetection(image);
-            pictureBox1.Image = sobel;
-        }
-
         private Bitmap sobelEdgeDetection(Bitmap image)
         {
             Bitmap gri = griYap(image);
-            Bitmap buffer=new Bitmap(image.Height,)
 
-            return image;
+            //Bitmap gri = new Bitmap(image,image.Width,image.Height);
+            Bitmap buffer = new Bitmap(image.Height, image.Width);
+            Color renk;
+            int valX, valY, gradient;
+
+            int[,] gX = new int[3, 3];
+            int[,] gY = new int[3, 3];
+
+            //Yatay yönde kenar     
+            gX[0, 0] = -1; gX[0, 1] = 0; gX[0, 2] = 1;
+            gX[1, 0] = -2; gX[1, 1] = 0; gX[1, 2] = 2;
+            gX[2, 0] = -1; gX[2, 1] = 0; gX[2, 2] = 1;
+
+            //Dikey yönde kenar
+            gY[0, 0] = -1; gY[0, 1] = -2; gY[0, 2] = -1;
+            gY[1, 0] = 0; gY[1, 1] = 0; gY[1, 2] = 0;
+            gY[2, 0] = 1; gY[2, 1] = 2; gY[2, 2] = 1;
+
+
+            for (int i = 0; i < gri.Height; i++)
+            {
+                for (int j = 0; j < gri.Width; j++)
+                {
+                    if (i == 0 || i == gri.Height - 1 || j == 0 || j == gri.Width - 1)
+                    {
+                        renk = Color.FromArgb(255, 255, 255);
+                        buffer.SetPixel(i, j, renk);
+                        valX = 0;
+                        valY = 0;
+                    }
+                    else
+                    {
+                        valX =
+                         gri.GetPixel(i - 1, j - 1).R * gX[0, 0] + gri.GetPixel(i - 1, j).R * gX[0, 1] + gri.GetPixel(i - 1, j + 1).R * gX[0, 2] +
+                         gri.GetPixel(i, j - 1).R * gX[1, 0] + gri.GetPixel(i, j).R * gX[1, 1] + gri.GetPixel(i, j + 1).R * gX[1, 2] +
+                         gri.GetPixel(i + 1, j - 1).R * gX[2, 0] + gri.GetPixel(i + 1, j).R * gX[2, 1] + gri.GetPixel(i + 1, j + 1).R * gX[2, 2];
+
+                        valY =
+                         gri.GetPixel(i - 1, j - 1).R * gY[0, 0] + gri.GetPixel(i - 1, j).R * gY[0, 1] + gri.GetPixel(i - 1, j + 1).R * gY[0, 2] +
+                         gri.GetPixel(i, j - 1).R * gY[1, 0] + gri.GetPixel(i, j).R * gY[1, 1] + gri.GetPixel(i, j + 1).R * gY[1, 2] +
+                         gri.GetPixel(i + 1, j - 1).R * gY[2, 0] + gri.GetPixel(i + 1, j).R * gY[2, 1] + gri.GetPixel(i + 1, j + 1).R * gY[2, 2];
+
+                        //valX =
+                        // gri.GetPixel(j - 1,i-1).R * gX[0, 0] + gri.GetPixel(j,i-1).R * gX[0, 1] + gri.GetPixel(j+1,i-1).R * gX[0, 2] +
+                        // gri.GetPixel(j - 1,i).R * gX[1, 0] + gri.GetPixel(j ,i).R * gX[1, 1] + gri.GetPixel(j + 1,i).R * gX[1, 2] +
+                        // gri.GetPixel(j - 1, i +1).R * gX[2, 0] + gri.GetPixel(j, i +1).R * gX[2, 1] + gri.GetPixel(j+1, i + 1).R * gX[2, 2];
+
+                        //valY =
+                        // gri.GetPixel(j - 1, i - 1).R * gY[0, 0] + gri.GetPixel(j, i - 1).R * gY[0, 1] + gri.GetPixel(j + 1, i - 1).R * gY[0, 2] +
+                        // gri.GetPixel(j - 1, i).R * gY[1, 0] + gri.GetPixel(j, i).R * gY[1, 1] + gri.GetPixel(j+1,i).R * gY[1, 2] +
+                        // gri.GetPixel(j - 1, i + 1).R * gY[2, 0] + gri.GetPixel(j, i + 1).R * gY[2, 1] + gri.GetPixel(j + 1, i + 1).R * gY[2, 2];
+
+                        gradient = (int)(Math.Abs(valX) + Math.Abs(valY));
+
+                        if (gradient < 0) { gradient = 0; }
+                        if (gradient > 255) { gradient = 255; }
+                        renk = Color.FromArgb(gradient, gradient, gradient);
+                        buffer.SetPixel(i, j, renk);
+                    }
+                }
+
+            }
+
+            return buffer;
+        }
+
+
+
+        private void btn_uygula_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0) // Gri seçildi.
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap gri = griYap(image);
+
+                pictureBox2.Image = gri;
+            }
+            if (comboBox1.SelectedIndex == 1) // Binary seçildi.
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap binary = binaryYap(image);
+                pictureBox2.Image = binary;
+            }
+            if (comboBox1.SelectedIndex == 2) // Sobel Kenar seçildi.
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap sobel = sobelEdgeDetection(image);
+
+                pictureBox2.Image = sobel;
+            }
+            if (comboBox1.SelectedIndex == 3) // Sobel Kenar seçildi.
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap median = medianFilter(image);
+
+                pictureBox2.Image = median;
+            }
+
+        }
+
+        private Bitmap medianFilter(Bitmap image)
+        {
+            Bitmap buffer = new Bitmap(image.Width, image.Height);
+            Color renk;
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    if (i == 0 || i == image.Height - 1 || j == 0 || j == image.Width - 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int ortanca = ortancayiBul(image, j, i);
+                        renk = Color.FromArgb(ortanca, ortanca, ortanca);
+                        buffer.SetPixel(j, i, renk);
+                    }
+
+                }
+            }
+
+
+            return buffer;
+        }
+
+        private int ortancayiBul(Bitmap image, int j, int i)
+        {
+            int[] dizi = new int[9];
+            Color renk;
+            int solust, ust, sagust, sol, sag, solalt, alt, sagalt;
+            sag = image.GetPixel(j + 1, i).R;
+            sagust = image.GetPixel(j + 1, i - 1).R;
+            ust = image.GetPixel(j, i - 1).R;
+            solust = image.GetPixel(j - 1, i - 1).R;
+            sol = image.GetPixel(j - 1, i).R;
+            solalt = image.GetPixel(j - 1, i + 1).R;
+            alt = image.GetPixel(j, i + 1).R;
+            sagalt = image.GetPixel(j + 1, i + 1).R;
+
+            dizi[0] = image.GetPixel(j, i).R;
+            dizi[1] = ust;
+            dizi[2] = sagust;
+            dizi[3] = sol;
+            dizi[4] = sag;
+            dizi[5] = solalt;
+            dizi[6] = alt;
+            dizi[7] = sagalt;
+
+
+            for (int k = 0; k < 8; k++)
+            {
+                for (int m = k+1; m < 9; m++)
+                {
+                    if (dizi[k] < dizi[m])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int temp = dizi[m];
+                        dizi[m] = dizi[k];
+                        dizi[k] = temp;
+
+
+
+
+                    }
+                }
+            }
+
+            return dizi[4];
         }
     }
 }
